67916ed2285fc67efcdcda0bdc17fe8952e4d555
Sarafanoff Dmitriy <dsarafanoff@apro-software.com>
2020-06-16 10:42:02 +0300

    Debug Commit 5 - Added thread assistance.

--- app/src/main/java/by/crispypigeon/secnotes/_views/main/MainActivity.java
+++ app/src/main/java/by/crispypigeon/secnotes/_views/main/MainActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import by.crispypigeon.secnotes.R;
+import by.crispypigeon.secnotes._presenters.main.MainPresenter;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainPresenter _mainPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        _mainPresenter = new MainPresenter(this);
+
+        configureEvents();
     }
+
+    private void configureEvents() {
+
+    }
+
+
 }
